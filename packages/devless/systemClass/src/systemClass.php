ace69b00260bcab3d2a0c1f290ea474abf97fd19
Edmond Mensah <kwasiamantin@gmail.com>
2018-07-23 02:37:30 +0000

    Update systemClass.php

--- packages/devless/systemClass/src/systemClass.php
+++ packages/devless/systemClass/src/systemClass.php
@@ -526,7 +526,7 @@ class devless
             $profileUpdate['password'] = Helper::password_hash($profileUpdate['password']);
         }
         unset($profileUpdate['id']);
-        return DB::table('users')->where('id', $id)->update($profileUpdate);
+        return $profileUpdate;
     }
 
     /**
